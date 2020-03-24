7ab997b988fea59a116627e4fa1eebf288c3c632
Krystal <Krystropolis@users.noreply.github.com>
2020-03-24 08:51:14 -0700

    remove google api key
    This key is not valid and a key should managed properly, especially if you have a publicly accessible repo.

--- geocode.py
+++ geocode.py
@@ -3,7 +3,7 @@ import json
 
 def getGeocodeLocation(inputString):
 	# this is bad practice but fine for our intended purposes
-	google_api_key = "AIzaSyCVd48YqZ0Oq0RoJuxfC5QIQaHElH5fZRI"
+	google_api_key = "yourgoogleapikeygoeshere"
 	locationString = inputString.replace(" ","+")
 	url = ('https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s'% (locationString, google_api_key))
 	h = httplib2.Http()
